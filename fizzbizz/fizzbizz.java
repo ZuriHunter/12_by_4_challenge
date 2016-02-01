42da9a5d11147492171692b30f8149399e9d1fc2
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-01-31 23:43:43 -0500

    Finished and corrected Java problem

--- fizzbizz/fizzbizz.java
+++ fizzbizz/fizzbizz.java
@@ -1,5 +1,23 @@
-/* Write a program that prints the numbers from 1 to 100. But for multiples of 
+/* Write a program that prints the numbers from 1 to 100. But for multiples of
 three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 For numbers which are multiples of both three and five print “FizzBuzz” */
 
 import math;
+
+public class FizzBizz{
+
+     public static void main(String []args){
+
+         for (int i = 0; i < 100; i ++){
+                 if (i % 3 == 0){
+                     System.out.println("Fizz");
+                 }
+                 else if (i % 5 == 0){
+                     System.out.println("Bizz");
+                 }
+                 else if (i % 5 == 0 && i % 3 == 0){
+                     System.out.println("FizzBizz");
+                 }
+             }
+         }
+     }
